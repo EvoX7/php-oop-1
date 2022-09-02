a95f653ae6fe8e2f3b266ed7bc2543bf144dfe6a
EvoX7 <creativesoultouch@gmail.com>
2022-09-02 17:51:18 +0200

    starting

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP oop 1</title>
+</head>
+<body>
+    <?php 
+require_once __DIR__ .  "/movie.php";
+
+$movie1 = new Movie('Matrix', 'It depicts a dystopian future in which humanity is unknowingly trapped inside a simulated reality, the Matrix','neo', 'dystopian', 7);
+
+
+
+
+?>
+
+
+</body>
+</html>
